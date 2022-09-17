afd0d79a05bdd9940b4fdd1b5296e45476e1352f
Rudolf Hrytseniak <Rudolf.Hrytseniak@thomsonreuters.com>
2022-09-17 12:23:02 +0200

    feat: configure cloudfront and update readme with links

--- tests/e2e/specs/ProductOrders.js
+++ tests/e2e/specs/ProductOrders.js
@@ -7,11 +7,11 @@ describe('view: ProductOrders', () => {
     });
 
     it('Layout: should be present', () => {
-        cy.shouldContainBaseLayout({ title: "My store!" })
+        cy.shouldContainBaseLayout({ title: "Whattablackhole's store" })
     });
 
     it('Footer: should contain copyright', () => {
-        cy.shouldContainFooter({ text: 'Copyright © My store!' })
+        cy.shouldContainFooter({ text: 'Copyright © Whattablackhole's store' })
     });
 
     it('Footer: should tell thank you', () => {
