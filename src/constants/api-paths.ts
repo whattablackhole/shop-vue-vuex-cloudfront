e7142f51c0069ab75fd87192a7f0518977868ceb
Rudolf Hrytseniak <Rudolf.Hrytseniak@thomsonreuters.com>
2022-10-09 22:03:57 +0200

    feat: update import url for task 5

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -4,7 +4,7 @@ export const API_PATHS = {
 	product:
 		'https://0uhsufj381.execute-api.eu-west-1.amazonaws.com/dev/products/',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://pwx2ddend5.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
